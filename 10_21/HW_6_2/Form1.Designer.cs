e8c445f2bfccf8f1f5551898b07c6f3f98fda417
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-10-23 23:59:47 +0800

    add HW 6-2

---
+++ 10_21/HW_6_2/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace F74116720_practice_6_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tab = new System.Windows.Forms.TabControl();
+            this.TelePage = new System.Windows.Forms.TabPage();
+            this.TeleLabel = new System.Windows.Forms.Label();
+            this.LogPage = new System.Windows.Forms.TabPage();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Tab.SuspendLayout();
+            this.TelePage.SuspendLayout();
+            this.LogPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Tab
+            // 
+            this.Tab.Controls.Add(this.TelePage);
+            this.Tab.Controls.Add(this.LogPage);
+            this.Tab.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Tab.Location = new System.Drawing.Point(0, 0);
+            this.Tab.Name = "Tab";
+            this.Tab.SelectedIndex = 0;
+            this.Tab.Size = new System.Drawing.Size(516, 452);
+            this.Tab.TabIndex = 0;
+            this.Tab.KeyUp += new System.Windows.Forms.KeyEventHandler(this.InputBox_KeyUp);
+            // 
+            // TelePage
+            // 
+            this.TelePage.Controls.Add(this.TeleLabel);
+            this.TelePage.Location = new System.Drawing.Point(4, 26);
+            this.TelePage.Name = "TelePage";
+            this.TelePage.Padding = new System.Windows.Forms.Padding(3);
+            this.TelePage.Size = new System.Drawing.Size(508, 422);
+            this.TelePage.TabIndex = 0;
+            this.TelePage.Text = "Telephone";
+            this.TelePage.UseVisualStyleBackColor = true;
+            // 
+            // TeleLabel
+            // 
+            this.TeleLabel.AutoSize = true;
+            this.TeleLabel.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TeleLabel.Location = new System.Drawing.Point(8, 15);
+            this.TeleLabel.Name = "TeleLabel";
+            this.TeleLabel.Size = new System.Drawing.Size(107, 24);
+            this.TeleLabel.TabIndex = 1;
+            this.TeleLabel.Text = "Telephone";
+            // 
+            // LogPage
+            // 
+            this.LogPage.Controls.Add(this.SaveBtn);
+            this.LogPage.Controls.Add(this.PathTextBox);
+            this.LogPage.Controls.Add(this.label1);
+            this.LogPage.Controls.Add(this.richTextBox1);
+            this.LogPage.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.LogPage.Location = new System.Drawing.Point(4, 26);
+            this.LogPage.Name = "LogPage";
+            this.LogPage.Padding = new System.Windows.Forms.Padding(3);
+            this.LogPage.Size = new System.Drawing.Size(508, 422);
+            this.LogPage.TabIndex = 1;
+            this.LogPage.Text = "Log";
+            this.LogPage.UseVisualStyleBackColor = true;
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(423, 22);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaveBtn.TabIndex = 3;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(109, 19);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(290, 25);
+            this.PathTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Save";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(452, 325);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 450);
+            this.Controls.Add(this.Tab);
+            this.Name = "Form1";
+            this.Text = "TT";
+            this.Tab.ResumeLayout(false);
+            this.TelePage.ResumeLayout(false);
+            this.TelePage.PerformLayout();
+            this.LogPage.ResumeLayout(false);
+            this.LogPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Tab;
+        private System.Windows.Forms.TabPage TelePage;
+        private System.Windows.Forms.Label TeleLabel;
+        private System.Windows.Forms.TabPage LogPage;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ 10_21/HW_6_2/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace F74116720_practice_6_2
+{
+
+    public partial class Form1 : Form
+    {
+        Button[] BtnArr = new Button[15];
+
+        const string ON_PHO = "📱";
+        const string OFF_PHO = "📵";
+        const string Path697 = @"../../audio/697.wav";
+        const string Path770 = @"../../audio/770.wav";
+        const string Path852 = @"../../audio/852.wav";
+        const string Path941 = @"../../audio/941.wav";
+        const string Path1209 = @"../../audio/1209.wav";
+        const string Path1336 = @"../../audio/1336.wav";
+        const string Path1477 = @"../../audio/1477.wav";
+
+        public static List<string> DB = new List<string>();
+        public static SoundPlayer MusicPlayer = new SoundPlayer();
+        void LoadBtn()
+        {
+            int init_x = 170;
+            int init_y = 80;
+            int span_x = 10;
+            int span_y = 10;
+            int wt = 50;
+            int ht = 50;
+            for(int i = 0; i < 15; i++)
+            {
+                BtnArr[i] = new Button();
+                BtnArr[i].Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+                if (i < 9)
+                {
+                    BtnArr[i].Text = (i + 1).ToString();
+                }
+                if( i<12)
+                {
+                    BtnArr[i].Click += new EventHandler(this.NormalClick);
+                }
+                BtnArr[i].SetBounds(init_x + (wt+span_x) * (i % 3), init_y + (ht+span_y) * (i / 3) + span_y,wt,ht);
+            }
+            BtnArr[9].Text = "*";
+            BtnArr[10].Text = "0";
+            BtnArr[11].Text = "#";
+            BtnArr[12].Text = "❌";
+            BtnArr[12].Click += new EventHandler(this.DeleteAll);
+            BtnArr[13].Text = ON_PHO;
+            BtnArr[13].Click += new EventHandler(this.PhoneClick);
+            BtnArr[14].Text = "⬅️";
+            BtnArr[14].Click += new EventHandler(this.DeleteOne);
+
+
+            foreach ( Button i in BtnArr)
+            {
+                TelePage.Controls.Add(i );
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            LoadBtn();
+        }
+        // Telephone button click event 
+        void DeleteOne(object sender,EventArgs e)
+        {
+            string str = TeleLabel.Text;
+            if (str.Length == 0) return;
+
+            TeleLabel.Text = str.Substring(0,str.Length-1);
+        }
+        void DeleteAll(object sender, EventArgs e)
+        {
+            TeleLabel.Text = "";
+        }
+        void PhoneClick(object sender, EventArgs e)
+        {
+            if( BtnArr[13].Text == ON_PHO)
+            {
+                DB.Add(TeleLabel.Text);
+                richTextBox1.Text += TeleLabel.Text + "\n";
+                BtnArr[13].Text = OFF_PHO;
+            }
+            else
+            {
+                TeleLabel.Text = "Telephone";
+                BtnArr[13].Text = ON_PHO;
+            }
+        }
+        void NormalClick(object sender, EventArgs e)
+        {
+            string str = ((Button)sender).Text;
+            string res = TeleLabel.Text + str;
+            TeleLabel.Text = res;
+
+            if( str=="0")
+            {
+                PlayAudio(3, 1);
+            }
+            else if( str=="1")
+            {
+                PlayAudio(0,0);
+            }
+            else if(str == "2")
+            {
+                PlayAudio(0,1);
+            }
+            else if (str == "3")
+            {
+                PlayAudio(0,2);
+            }
+            else if( str =="4")
+            {
+                PlayAudio(1,0);
+            }
+            else if( str =="5")
+            {
+                PlayAudio(1,1);
+            }
+            else if (str == "6")
+            {
+                PlayAudio(1,2);
+            }
+            else if (str == "7")
+            {
+                PlayAudio(2,0);
+            }
+            else if (str == "8")
+            {
+                PlayAudio(2,1);
+            }
+            else if (str == "9")
+            {
+                PlayAudio(2,2);
+            }
+            else if( str == "*")
+            {
+                PlayAudio(2, 0);
+            }
+            else if( str == "#")
+            {
+                PlayAudio(2, 2);
+            }
+
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            string path = PathTextBox.Text;
+            if( path.Length == 0)
+            {
+                MessageBox.Show("Empty String", "Error", MessageBoxButtons.OK);
+            }
+            else
+            {
+                File.WriteAllText(path, richTextBox1.Text);
+                MessageBox.Show("Done\n"+path, "Successful", MessageBoxButtons.OK);
+            }
+            
+        }
+
+        void PlayAudio(int i,int j)
+        {
+            if( i == 0)
+            {
+                MusicPlayer.SoundLocation = Path697;
+                MusicPlayer.Load();
+                MusicPlayer.Play();
+                if (j == 0)
+                { 
+                    MusicPlayer.SoundLocation = Path1209;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+                else if (j == 1)
+                {
+                    MusicPlayer.SoundLocation = Path1336;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+                else if(j ==2)
+                {
+                    MusicPlayer.SoundLocation = Path1477;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+            }
+            else if( i== 1)
+            {
+                MusicPlayer.SoundLocation = Path770;
+                MusicPlayer.Load();
+                MusicPlayer.Play();
+                if (j == 0)
+                {
+                    MusicPlayer.SoundLocation = Path1209;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+                else if (j == 1)
+                {
+                    MusicPlayer.SoundLocation = Path1336;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+                else if (j == 2)
+                {
+                    MusicPlayer.SoundLocation = Path1477;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+            }
+            else if(i == 2)
+            {
+                MusicPlayer.SoundLocation = Path852;
+                MusicPlayer.Load();
+                MusicPlayer.Play();
+                if (j == 0)
+                {
+                    MusicPlayer.SoundLocation = Path1209;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+                else if (j == 1)
+                {
+                    MusicPlayer.SoundLocation = Path1336;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+                else if (j == 2)
+                {
+                    MusicPlayer.SoundLocation = Path1477;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+            }
+            else if (i == 3)
+            {
+                MusicPlayer.SoundLocation = Path941;
+                MusicPlayer.Load();
+                MusicPlayer.Play();
+                if (j == 0)
+                {
+                    MusicPlayer.SoundLocation = Path1209;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+                else if (j == 1)
+                {
+                    MusicPlayer.SoundLocation = Path1336;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+                else if (j == 2)
+                {
+                    MusicPlayer.SoundLocation = Path1477;
+                    MusicPlayer.Load();
+                    MusicPlayer.Play();
+                }
+            }
+        }
+        
+
+        private void InputBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Tab.SelectedIndex == 1) return;
+
+            if( e.Modifiers == Keys.Shift)
+            {
+                if( e.KeyCode == Keys.D3)
+                {
+                    TeleLabel.Text = TeleLabel.Text + "#";
+                    PlayAudio(3, 2);
+                }
+                else if( e.KeyCode == Keys.D8)
+                {
+                    TeleLabel.Text = TeleLabel.Text + "*";
+                    PlayAudio(3, 0);
+                }
+            }
+            else if( e.KeyCode == Keys.Enter)
+            {
+                if (BtnArr[13].Text == ON_PHO)
+                {
+                    DB.Add(TeleLabel.Text);
+                    richTextBox1.Text += TeleLabel.Text + "\n";
+                    BtnArr[13].Text = OFF_PHO;
+                }
+                else
+                {
+                    TeleLabel.Text = "Telephone";
+                    BtnArr[13].Text = ON_PHO;
+                }
+            }
+            else if( e.KeyCode == Keys.Back)
+            {
+                string str = TeleLabel.Text;
+                if (str.Length != 0)
+                {
+                    TeleLabel.Text = str.Substring(0, str.Length - 1);
+                }
+            }
+            else if( e.KeyCode>= Keys.D0 && e.KeyCode<=Keys.D9)
+            {
+                TeleLabel.Text = TeleLabel.Text + ((char)e.KeyCode).ToString();
+                if( e.KeyCode == Keys.D0)
+                {
+                    PlayAudio(3, 1);
+                }
+                else if (e.KeyCode == Keys.D1)
+                {
+                    PlayAudio(0,0);
+                }
+                else if (e.KeyCode == Keys.D2)
+                {
+                    PlayAudio(0, 1);
+                }
+                else if (e.KeyCode == Keys.D3)
+                {
+                    PlayAudio(0, 2);
+                }
+                else if (e.KeyCode == Keys.D4)
+                {
+                    PlayAudio(1, 0);
+                }
+                else if (e.KeyCode == Keys.D5)
+                {
+                    PlayAudio(1, 1);
+                }
+                else if (e.KeyCode == Keys.D6)
+                {
+                    PlayAudio(1, 2);
+                }
+                else if (e.KeyCode == Keys.D7)
+                {
+                    PlayAudio(2, 0);
+                }
+                else if (e.KeyCode == Keys.D8)
+                {
+                    PlayAudio(2, 1);
+                }
+                else if (e.KeyCode == Keys.D9)
+                {
+                    PlayAudio(2, 2);
+                }
+            }
+        }
+        
+    }
+}
