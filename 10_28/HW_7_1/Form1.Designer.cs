51edceeb7db52d582e2c9ef6d31bf24922fc1bbd
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-10-31 13:37:02 +0800

    Finish HW 7-1

---
+++ 10_28/HW_7_1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace F74116720_practice_7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddTaskBtn = new System.Windows.Forms.Button();
+            this.TxtBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontSizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AddTaskBtn
+            // 
+            this.AddTaskBtn.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.AddTaskBtn.Location = new System.Drawing.Point(1165, 802);
+            this.AddTaskBtn.Name = "AddTaskBtn";
+            this.AddTaskBtn.Size = new System.Drawing.Size(216, 60);
+            this.AddTaskBtn.TabIndex = 0;
+            this.AddTaskBtn.Text = "Add Task";
+            this.AddTaskBtn.UseVisualStyleBackColor = true;
+            this.AddTaskBtn.Click += new System.EventHandler(this.AddTaskBtn_Click);
+            // 
+            // TxtBox
+            // 
+            this.TxtBox.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TxtBox.Location = new System.Drawing.Point(53, 94);
+            this.TxtBox.Name = "TxtBox";
+            this.TxtBox.ReadOnly = true;
+            this.TxtBox.Size = new System.Drawing.Size(1328, 674);
+            this.TxtBox.TabIndex = 1;
+            this.TxtBox.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1523, 42);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(65, 38);
+            this.fIleToolStripMenuItem.Text = "FIle";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontSizeToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(80, 38);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // fontSizeToolStripMenuItem
+            // 
+            this.fontSizeToolStripMenuItem.Name = "fontSizeToolStripMenuItem";
+            this.fontSizeToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.fontSizeToolStripMenuItem.Text = "Font Size";
+            this.fontSizeToolStripMenuItem.Click += new System.EventHandler(this.fontSizeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1523, 874);
+            this.Controls.Add(this.TxtBox);
+            this.Controls.Add(this.AddTaskBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Todo List";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddTaskBtn;
+        private System.Windows.Forms.RichTextBox TxtBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontSizeToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ 10_28/HW_7_1/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace F74116720_practice_7_1
+{
+    public partial class Form1 : Form
+    {
+        // Ask Form obj
+        public static Form AskForm = new Form();
+        public static Button AskOkBtn = new Button();
+        public static Button AskCancelBtn = new Button();
+        public static Label AskLabel = new Label();
+        public static TextBox AskTxtBox= new TextBox();
+        // other
+        public static string NewTask;
+        public static string Status;
+        public static string fileContent;
+        public static string filePath;
+
+
+       string GetCurrentTasks()
+        {
+            return TxtBox.Text;
+        }
+        void ResetCurrentTasks()
+        {
+            TxtBox.Text = "";
+        }
+        void SetCurrentTasks(string str)
+        {
+            TxtBox.Text = str;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            /* Ask form  */
+            // event handler
+            AskForm.FormClosing += AskForm_FormClosing;
+            AskForm.ClientSize = new System.Drawing.Size(800, 200);
+
+            // obj init
+            AskLabel.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            AskLabel.Location = new System.Drawing.Point(50, 50);
+            AskLabel.Size = new System.Drawing.Size(100, 50);
+            AskLabel.Text = "Event Name";
+
+            AskTxtBox.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            AskTxtBox.Location = new System.Drawing.Point(150, 50);
+            AskTxtBox.Size = new System.Drawing.Size(580, 50);
+
+            AskOkBtn.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            AskOkBtn.Location = new System.Drawing.Point(630, 120);
+            AskOkBtn.Size = new System.Drawing.Size(100, 35);
+            AskOkBtn.Text = "Confirm";
+            AskOkBtn.Click += AskOkBtn_Click;
+
+            AskCancelBtn.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            AskCancelBtn.Location = new System.Drawing.Point(520, 120);
+            AskCancelBtn.Size = new System.Drawing.Size(100, 35);
+            AskCancelBtn.Text = "Cancel";
+            AskCancelBtn.Click += AskCalcelBtn_Click;
+
+            AskForm.Controls.Add(AskLabel);
+            AskForm.Controls.Add(AskTxtBox);
+            AskForm.Controls.Add(AskOkBtn);
+            AskForm.Controls.Add(AskCancelBtn);
+        }
+
+        private void AskOkBtn_Click(object sender, EventArgs e)
+        {
+            NewTask = AskTxtBox.Text;
+            if( NewTask.Length == 0)
+            {
+                MessageBox.Show("Please fill in the task name", "Error", MessageBoxButtons.OK);
+            }
+            else
+            {
+                AskTxtBox.Text = "";
+                AskForm.Close();
+                TxtBox.Text += NewTask + "\n";
+            }
+        }
+        private void AskCalcelBtn_Click(object sender, EventArgs e)
+        {
+            NewTask = "";
+            AskTxtBox.Text = "";
+            AskForm.Close();
+        }
+        private void AskForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Enabled = true;
+            AskForm.Hide();
+        }
+        
+
+        private void AddTaskBtn_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            AskForm.Show();
+        }
+        
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Status = ((ToolStripMenuItem)sender).Text;
+            ResetCurrentTasks();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Status = ((ToolStripMenuItem)sender).Text;
+
+            fileContent = string.Empty;
+            filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Todo files (*.todo)|*.todo|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 3;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            SetCurrentTasks(fileContent);
+            //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if( Status == "New")
+            {
+                SaveFileAs();
+            }
+            else
+            {
+                FileStream fs = File.OpenWrite(filePath);
+                
+                byte[] bytes = Encoding.UTF8.GetBytes( GetCurrentTasks() );
+                fs.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        void SaveFileAs()
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Todo files (*.todo)|*.todo|txt files (*.txt)|*.txt";
+            saveFileDialog1.Title = "Save Todo List";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                byte[] bytes = Encoding.UTF8.GetBytes(GetCurrentTasks());
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1: // todo
+                        fs.Write(bytes, 0, bytes.Length);
+                        break;
+
+                    case 2: // txt
+                        fs.Write(bytes, 0, bytes.Length);
+                        break;
+                        
+                }
+
+                fs.Close();
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileAs();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void fontSizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog f = new FontDialog();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                TxtBox.Font = f.Font;
+            }
+        }
+    }
+}
